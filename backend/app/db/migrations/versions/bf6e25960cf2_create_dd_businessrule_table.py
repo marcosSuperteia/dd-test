93da91fe5000f1eb627bd065c167739a3767ce88
marcosSupertia <marcos.nascimento@smarti.com.br>
2023-11-16 08:56:27 -0300

    feat: :sparkles: Regras de negócio

---
+++ backend/app/db/migrations/versions/bf6e25960cf2_create_dd_businessrule_table.py
@@ -0,0 +1,24 @@
+"""create_dd_businessrule_table
+
+Revision ID: bf6e25960cf2
+Revises: ac332c906e54
+Create Date: 2023-11-16 11:55:13.905424
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bf6e25960cf2'
+down_revision = 'ac332c906e54'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
